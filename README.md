4b0ecdb05f9cd5f7c4f2a260c94d0f96e8b92832
Carl Janz <carl.janz@gmail.com>
2019-02-28 16:58:50 -0800

    worked on game behavior

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Psychic-Game
+# Word Guess Game
+This game challenges a player to guess the random word the computer has chosen. They do this by typing a letter key, if the letter is in the the chosen word the matching letters are revealed.
+
+TO DO:
+- Create word bank
+- Pick theme
+- get some sfx
+- more things
--- assets/javascsript/game.js
+++ assets/javascsript/game.js
@@ -1,41 +1,80 @@
-const validKeys = /[a-z]/;
-const wordBank = ["baby", "rope"];
-let gameRunning = false;
-let guessedLetters;
-let chosenWord;
+const VALIDKEYS = /[a-z]/;
+const WORDBANK = ["baby", "rope"];
+const STARTINGLIVES = 10
 
+let game = {
 
-function startGame() {
-    guessedLetters = [];
-    gameRunning = true;
-    chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(chosenWord);
-}
+    gameRunning: false,
+    guessedLetters: [],
+    incorrectGuesses: [],
+    chosenWord: null,
+    livesLeft: STARTINGLIVES,
+    charactersRevealed: 0,
 
-function endGame(result) {
-    switch (result) {
-        case "win":
-            console.log("You Won!");
-            gameRunning = false;
-            break;
-        case "loss":
+    takeInput: function(input) {
+        keyPressed = input.key.toLowerCase();
+
+        //If the game is not currently running, this starts a new game and eats the keystroke.
+        if (!this.gameRunning) {
+            this.startGame();
+
+            // If the game is already running this tests to ensure that the keystroke is valid (a-z). If so, pass it along to gameTurn.
+            // !guessedLetters.Includes discards any keys that have already been guessed.
+            // The keyPressed.length evaluation is to discard special key keystrokes (example: Tab or Enter).
+        } else if (VALIDKEYS.test(keyPressed) && !this.guessedLetters.includes(keyPressed) && keyPressed.length === 1) {
+            this.guessedLetters.push(keyPressed);
+            this.takeTurn(keyPressed);
+            this.checkEndGame();
+        }
+    },
+
+    reset: function() {
+        this.guessedLetters = [];
+        this.chosenWord = null;
+        this.livesLeft = STARTINGLIVES;
+        this.charactersRevealed = 0;
+    },
+
+    startGame: function() {
+        console.log("Game Starting");
+
+        this.reset();
+        this.gameRunning = true;
+        this.chosenWord = WORDBANK[Math.floor(Math.random() * WORDBANK.length)];
+    },
+
+    takeTurn: function(guessedLetter) {
+        for (let i = 0; i < this.chosenWord.length; i++) {
+            if (this.chosenWord[i] === guessedLetter) {
+                this.charactersRevealed++;
+            }
+        }
+        this.livesLeft--;
+        this.printInfo();
+    },
+
+    checkEndGame: function() {
+        if (this.livesLeft <= 0) {
             console.log("You Lost!");
-            gameRunning = false;
-            break;
-        default:
-            console.log("somthing went wrong");
+            this.gameRunning = false;
+        }
+
+        if (this.charactersRevealed === this.chosenWord.length) {
+            console.log("You Won!");
+            this.gameRunning = false;
+        }
+    },
+
+    //TEMPORARY DEBUG METHOD!!!! DELETE BEFORE SUBMITTING
+    printInfo: function() {
+        console.log("----------------");
+        console.log("lives left:", this.livesLeft);
+        console.log("Chosen Word:", this.chosenWord);
+        console.log("Guessed Letters:", this.guessedLetters);
+        console.log("Letters Revealed:", this.charactersRevealed);
+        console.log("Game Running:", this.gameRunning);
+        console.log("+++++++++++++++++");
     }
 }
 
-function gameTurn(guessedLetter) {
-
-}
-
-document.onkeyup = function(event) {
-    const keyPressed = event.key.toLowerCase();
-    if (!gameRunning) {
-        startGame();
-    } else if (validKeys.test(keyPressed) && keyPressed.length === 1) {
-        console.log("key", keyPressed);
-    }
-}
+document.onkeyup = function(event) { game.takeInput(event) };
